09f8d82444313af80b3812fef1f02bcefef1a449
Zoltan Stal <zstal@users.noreply.github.com>
2024-04-15 12:16:49 -0400

    changes to bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+#
+# This script bootstraps a fresh macOS install
+#
+
 set -euo pipefail
 
-#
-# This script bootstraps a fresh macOS install from scratch
-#
+# clear the Dock & force a UI refresh
+defaults write com.apple.dock persistent-apps -array && killall Dock
 
-# clear all icons from the dock
-defaults write com.apple.dock persistent-apps -array
+echo '🔄 Configuring macOS settings...'
+./defaults/macsetup.sh
 
-echo '🔄 Installing dotfiles via dotbot...'
+echo '🔄 Installing dotfiles...'
 ./install-dotfiles
 
-echo '🔄 Installing homebrew...'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '🔄 Ensuring homebrew is installed...'
+command -v brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo '🔄 Installing CLI tools via homebrew...'
 /opt/homebrew/bin/brew bundle install --file homebrew/cli.brewfile --verbose
@@ -23,6 +26,3 @@ echo '🔄 Installing GUI apps via homebrew...'
 
 echo '🔄 Associating extensions with apps...'
 /opt/homebrew/bin/duti -v defaults/Dutifile
-
-echo '🔄 Tweaking macOS config...'
-./defaults/macsetup.sh
