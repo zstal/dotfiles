0d28bc9900ec1eb1fcf5c466ab06e6c89e5f05b7
Zoltan Stal <zstal@users.noreply.github.com>
2025-10-13 16:45:37 -0400

    shortcut change

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -43,7 +43,7 @@ hs.fnutils.each({
   { key = '`', app = 'Sublime Text' },
   { key = '1', app = 'Terminal' },
   { key = '2', app = 'Finder' },
-  { key = '3', app = 'Fork' },
+  { key = '3', app = 'Firefox' },
 }, function(item)
 
 local appActivation = function()
